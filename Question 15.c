7b94b2f2c0412658f424a7314bd649149f6108f0
Shan1062004 <144580937+Shan1062004@users.noreply.github.com>
2024-10-20 15:45:15 +0530

    Add files via upload

---
+++ Question 15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void printPattern(int rows, int cols) {
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            if (i == 1 || i == rows || j == 1 || j == cols) {
+                printf("1");
+            } else {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    printPattern(rows, cols);
+    return 0;
+}
